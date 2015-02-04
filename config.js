020092450115cdffb1a2abea577bc0876fe6b65c
uf0 <scnepa222@gmail.com>
2015-02-04 14:21:47 +0100

    added config and download scripts

---
+++ config.js
@@ -0,0 +1,6 @@
+
+//add your configuration
+
+var config = {url : 'http://188.226.235.250/getdataapp'};
+
+module.exports = config;
---
+++ videodata.js
@@ -0,0 +1,23 @@
+var request = require('request'),
+    fs = require('fs'),
+    CONFIG = require('./config.js');
+
+var options = {
+  url: CONFIG.url
+}
+
+
+request(options, function(err, res, body) {
+  if(err){console.log(err)}
+  else{ 
+    var data = JSON.parse(body)
+    fs.writeFile("data/routes.json", JSON.stringify(data, null, 2), function(err) {
+      if(err) {
+        console.log(err);
+      }else{
+        console.log("file saved!")
+      }
+    });
+  }
+}); 
+})
